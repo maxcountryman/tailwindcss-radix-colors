f4730e24e6807ec2de6df1e288bc44fcf8a76a10
mrcaidev <mrcaidev@gmail.com>
2023-01-04 20:32:17 +0800

    chore: add integrations of astro

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,10 @@
+import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  integrations: [sitemap(), solid(), tailwind()],
   site: "https://tailwindcss-radix-colors.mrcai.dev",
   vite: {
     build: {
