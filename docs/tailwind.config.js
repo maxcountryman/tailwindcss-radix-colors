936bd77643958f33064388be376b8d1b8541f651
mrcaidev <mrcaidev@gmail.com>
2023-01-04 20:43:12 +0800

    chore: other dependencies for astrojs

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   content: ["src/{components,layouts,pages}/**/*.{astro,tsx}"],
   darkMode: "class",
-  plugins: [require("tailwindcss-radix-colors")],
+  plugins: [
+    require("tailwindcss-radix-colors"),
+    require("@tailwindcss/typography"),
+  ],
 };
