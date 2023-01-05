7bb4ba6a8f4d3b48cb1b66a7850e0282ff8acd8e
mrcaidev <mrcaidev@gmail.com>
2023-01-05 17:14:48 +0800

    docs: use inter font

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["src/{components,layouts,pages}/**/*.{astro,tsx}"],
   darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
   plugins: [
     require("tailwindcss-radix-colors"),
     require("@tailwindcss/typography"),
