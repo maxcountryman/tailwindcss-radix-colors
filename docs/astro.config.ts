4978b81cf89a626fe94415831c1f6c9ba9373cab
mrcaidev <mrcaidev@gmail.com>
2023-01-05 14:42:16 +0800

    chore: remove @astrojs/sitemap

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,10 +1,9 @@
-import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [sitemap(), solid(), tailwind()],
+  integrations: [solid(), tailwind()],
   site: "https://tailwindcss-radix-colors.mrcai.dev",
   vite: {
     build: {
